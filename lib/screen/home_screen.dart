94a32bbfc29898e8d76ec792bdcbdbb6f3983d3b
jake.kyo <jake.kyo@kakaocorp.com>
2024-09-15 18:19:13 +0900

    레이아웃 작업하기

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -6,7 +6,29 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
+      body: SafeArea(
+        child: Column(
+          children: [
+            // 제목과 아이콘 버튼 영역
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text('제목'),
+                IconButton(onPressed: () {}, icon: Icon(Icons.settings))
+              ],
+            ),
+            // 숫자 영역
+            Expanded(
+              child: Text('''123
+              456
+              789
+              '''),
+            ),
+            // 버튼 영역
+            ElevatedButton(onPressed: () {}, child: Text('생성하기!'))
+          ],
+        ),
+      ),
     );
   }
 }
